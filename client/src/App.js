b430a85c2f7eab518450a44bde3c35bd1a0d1c0b
Aaron Sanchez <saarong12@gmail.com>
2022-10-20 17:59:06 -0500

    Routes are set up with a home page component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import './App.css'
 
+import Home from './Components/Home'
+import { useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+
 function App() {
   return (
     <>
-      <div>Siege</div>
+      <header>
+        <h1>Nav goes here</h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </>
   )
 }
