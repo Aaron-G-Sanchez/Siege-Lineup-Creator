1a6802a64369af62646aee3a5a18420b5850b18a
Aaron Sanchez <saarong12@gmail.com>
2022-10-25 10:31:30 -0500

    Delete button created and works with route

--- client/src/Components/DeleteTeamMember.jsx
+++ client/src/Components/DeleteTeamMember.jsx
@@ -1,9 +1,20 @@
-const DeleteTeamMember = () => {
+import axios from "axios"
+
+const DeleteTeamMember = (props) => {
+
+const handleClick = async () => {
+      let id = props.id
+      try {
+        await axios.delete(`http://localhost:3001/teamMembers/delete/${id}`)
+      } catch (err) {
+        console.log(err)
+      }
+    } 
 
   return (
     <>
     <div>
-      <button>X</button>
+      <button onClick={handleClick}>X</button>
     </div>
     </>
   )
--- client/src/pages/TeamSelectionCard.js
+++ client/src/pages/TeamSelectionCard.js
@@ -1,16 +1,19 @@
 import DeleteTeamMember from '../components/DeleteTeamMember'
+import axios from 'axios'
 
 const TeamSelectionCard = (props) => {
   return (
     <>
-      <div className="selection-card" onClick={props.onClick}>
-        {props.op ? (
-          <div className="created-op-info">
-            <h2>{props.op.name}</h2>
-            <p>{props.op._id}</p>
-            {<DeleteTeamMember />}
-          </div>
-        ) : null}
+      <div className="selection-options">
+        <div className="selection-card" onClick={props.onClick}>
+          {props.op ? (
+            <div className="created-op-info">
+              <h2>{props.op.name}</h2>
+              <p>{props.op._id}</p>
+            </div>
+          ) : null}
+        </div>
+        {props.op ? <DeleteTeamMember id={props.op._id} /> : null}
       </div>
     </>
   )
